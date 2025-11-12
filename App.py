ab7ee9ef98b10594cc182b704c8071144de7ebe5
Suksan tuaklang <flukzaza1551@gmail.com>
2025-11-12 12:04:51 +0700

    Update App.py

--- App.py
+++ App.py
@@ -24,7 +24,10 @@ if manpower_file and stylelist_file and raweff_file and mastergwc_file and indiv
     individual_eff = pd.read_csv(individual_eff_file)
 
     st.success("✅ ทุกไฟล์โหลดสำเร็จแล้ว!")
+for df in [manpower, stylelist, raweff, individual_eff, master_gwc]:
+    df.columns = df.columns.str.lower().str.strip()
 
+    
     # ---------------------------------------------------------
     # 3. Merge เพื่อหา missing eff ก่อน
     # ---------------------------------------------------------
